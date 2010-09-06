6c79738a2fd80aff108baec7e0f6a907702c6af9
eremite <eremite@gmail.com>
2010-09-06 16:53:18 -0600

    Auto-generate a table of contents.

--- converter.rb
+++ converter.rb
@@ -8,19 +8,25 @@ require 'active_support/all'
 LAYOUT = 'layouts/site.html.liquid'
 
 layout = Liquid::Template.parse(File.read(LAYOUT))
-files = Dir['**/_*.html'] + Dir['**/*.{txt,text,md,markdown,mdown,markdn,mdml}']
+files = Dir['**/*.{txt,text,md,markdown,mdown,markdn,mdml}']
+skip_index = false
+toc = {}
 
-files = ARGV unless ARGV.empty?
+unless ARGV.empty?
+  files = ARGV
+  skip_index = true
+end
 
 files.each do |file|
   raise "File not found: #{file}" unless File.exists?(file)
-  outfile = "#{file}"
-  if File.basename(file).starts_with?('_')
-    outfile.sub!(Regexp.new("#{File.basename(file)}$"), File.basename(file).sub(/^_/, ''))
-  end
-  outfile.sub!(/\.[^.]*$/, '.html')
+  outfile = file.sub(/\.[^.]*$/, '.html')
   raise "File #{file} would be overwritten." if outfile == file
   puts "Converting #{file}"
+  filename, *category  = file.split('/').reverse
+  category = category.join('-')
+  toc[category] ||= []
+  parts = filename.split('.').first.split('-')
+  name, title = parts[0..-2].join(', ').titleize, parts.last.titleize
   File.open(outfile, 'w') do |f|
     content = File.read(file)
     links = {}
@@ -31,13 +37,41 @@ files.each do |file|
     if File.exists?("#{file.sub(/\.[^\.]+$/, '')}.pdf")
       links[:pdf] = File.basename("#{file.sub(/\.[^\.]+$/, '')}.pdf")
     end
-    content =~ /<h1[^>]*>(.*)<\/h1>/i
+    toc[category] << {
+      :name => name,
+      :href => outfile,
+      :text => title,
+    }
     f << layout.render({
-      :title => $1,
+      :title => title,
       :relative_root => '.' * (file.scan('/').size + 1),
       :content => content,
       :text_link => links[:text],
       :pdf_link => links[:pdf],
+    }.with_indifferent_access)
+  end
+end
+
+unless skip_index
+  puts "Creating Index"
+  outfile = "index.html"
+  File.open(outfile, 'w') do |f|
+    content = ''
+    toc.each do |category, links|
+      next if category.blank?
+      content << <<-HEREDOC
+      <div class="category">
+        <h2>#{category.titleize}</h2>
+        <ul>
+          #{links.sort_by {|a| a[:title].to_s}.map {|a| %Q%<li>#{a[:name]}<br /><a href="#{a[:href]}">#{a[:text]}</a></li>%}}
+        </ul>
+      </div>
+      HEREDOC
+    end
+    f << layout.render({
+      :title => "Family History of Mark and Bodell Esplin",
+      :relative_root => '.',
+      :content => content,
     }.with_indifferent_access)
   end
 end
